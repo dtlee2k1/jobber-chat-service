36ff196aee1e50f0c5201b6ae73cf8a0bfb31246
Le Duc Thai <leducthai2001@gmail.com>
2024-03-18 20:53:51 +0700

    handle message service methods

---
+++ src/services/message.service.ts
@@ -0,0 +1,154 @@
+import { ConversationModel } from '@chat/models/conversation.schema';
+import { MessageModel } from '@chat/models/message.schema';
+import { publishDirectMessage } from '@chat/queues/message.producer';
+import { chatChannel, socketIOChatObject } from '@chat/server';
+import { IConversationDocument, IMessageDocument } from '@dtlee2k1/jobber-shared';
+
+export async function createConversation(conversationId: string, sender: string, receiver: string) {
+  await ConversationModel.create({
+    conversationId,
+    senderUsername: sender,
+    receiverUsername: receiver
+  });
+}
+
+export async function addMessage(data: IMessageDocument) {
+  const message: IMessageDocument = MessageModel.create({ data }) as IMessageDocument;
+
+  if (message.hasOffer) {
+    const emailMessageDetails = {
+      sender: data.senderUsername,
+      amount: data.offer?.price,
+      buyerUsername: data.receiverUsername?.toLowerCase(),
+      sellerUsername: data.senderUsername?.toLowerCase(),
+      title: data.offer?.gigTitle,
+      description: data.offer?.description,
+      deliveryInDays: data.offer?.deliveryInDays,
+      template: 'offer'
+    };
+    // send email
+    await publishDirectMessage(
+      chatChannel,
+      'jobber-order-notification',
+      'order-email',
+      JSON.stringify(emailMessageDetails),
+      'Order email sent to notification service'
+    );
+
+    socketIOChatObject.emit('message_received', message);
+    return message;
+  }
+}
+
+export async function getConversation(sender: string, receiver: string) {
+  const query = {
+    $or: [
+      { senderUsername: sender, receiverUsername: receiver },
+      { senderUsername: receiver, receiverUsername: sender }
+    ]
+  };
+
+  const conversation: IConversationDocument[] = (await ConversationModel.aggregate([{ $match: query }])) as IConversationDocument[];
+  return conversation;
+}
+
+export async function getUserConversationList(username: string) {
+  const query = {
+    $or: [{ senderUsername: username }, { receiverUsername: username }]
+  };
+  const messages: IMessageDocument[] = await MessageModel.aggregate([
+    { $match: query },
+    {
+      $group: {
+        _id: '$conversationId',
+        result: { $top: { output: '$$ROOT', sortBy: { createdAt: -1 } } }
+      }
+    },
+    {
+      $project: {
+        _id: '$result._id',
+        conversationId: '$result.conversationId',
+        sellerId: '$result.sellerId',
+        buyerId: '$result.buyerId',
+        receiverUsername: '$result.receiverUsername',
+        receiverPicture: '$result.receiverPicture',
+        senderUsername: '$result.senderUsername',
+        senderPicture: '$result.senderPicture',
+        body: '$result.body',
+        file: '$result.file',
+        gigId: '$result.gigId',
+        isRead: '$result.isRead',
+        hasOffer: '$result.hasOffer',
+        createdAt: '$result.createdAt'
+      }
+    }
+  ]);
+  return messages;
+}
+
+export async function getMessages(sender: string, receiver: string) {
+  const query = {
+    $or: [
+      { senderUsername: sender, receiverUsername: receiver },
+      { senderUsername: receiver, receiverUsername: sender }
+    ]
+  };
+
+  const messages: IMessageDocument[] = await MessageModel.aggregate([
+    {
+      $match: query
+    },
+    { $sort: { createdAt: 1 } }
+  ]);
+  return messages;
+}
+
+export async function getUserMessages(messageConversationId: string) {
+  const messages: IMessageDocument[] = await MessageModel.aggregate([
+    {
+      $match: { conversationId: messageConversationId }
+    },
+    { $sort: { createdAt: 1 } }
+  ]);
+  return messages;
+}
+
+export async function updateOffer(messageId: string, type: string) {
+  const message: IMessageDocument = (await MessageModel.findOneAndUpdate(
+    { _id: messageId },
+    {
+      $set: {
+        [`offer.${type}`]: true // type = "accepted" | "canceled"
+      }
+    },
+    { new: true }
+  )) as IMessageDocument;
+  return message;
+}
+
+export async function markMessageAsRead(messageId: string) {
+  const message: IMessageDocument = (await MessageModel.findOneAndUpdate(
+    { _id: messageId },
+    {
+      $set: { isRead: true }
+    },
+    { new: true }
+  )) as IMessageDocument;
+
+  socketIOChatObject.emit('message_updated', message);
+  return message;
+}
+
+export async function markManyMessagesAsRead(sender: string, receiver: string, messageId: string) {
+  await MessageModel.updateMany(
+    { senderUsername: sender, receiverUsername: receiver, isRead: false },
+    {
+      $set: { isRead: true }
+    }
+  );
+
+  const message: IMessageDocument = (await MessageModel.findOne({ _id: messageId }).exec()) as IMessageDocument;
+
+  socketIOChatObject.emit('message_updated', message);
+  return message;
+}
