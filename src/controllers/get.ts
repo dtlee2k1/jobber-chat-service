a99e9be1cca5fd5c431a4ee23b58ff60c3437b3b
Le Duc Thai <leducthai2001@gmail.com>
2024-03-19 09:47:06 +0700

    handle update & get controllers

---
+++ src/controllers/get.ts
@@ -0,0 +1,48 @@
+import { getConversation, getMessages, getUserConversationList, getUserMessages } from '@chat/services/message.service';
+import { IConversationDocument, IMessageDocument } from '@dtlee2k1/jobber-shared';
+import { NextFunction, Request, Response } from 'express';
+import { StatusCodes } from 'http-status-codes';
+
+export async function conversation(req: Request, res: Response, _next: NextFunction) {
+  const { senderUsername, receiverUsername } = req.params;
+
+  const conversations: IConversationDocument[] = await getConversation(senderUsername, receiverUsername);
+
+  res.status(StatusCodes.OK).json({
+    message: 'Get chat conversation successfully',
+    conversations
+  });
+}
+
+export async function conversationList(req: Request, res: Response, _next: NextFunction) {
+  const { username } = req.params;
+
+  const messages: IMessageDocument[] = await getUserConversationList(username);
+
+  res.status(StatusCodes.OK).json({
+    message: 'Get conversation list successfully',
+    conversation: messages
+  });
+}
+
+export async function messages(req: Request, res: Response, _next: NextFunction) {
+  const { senderUsername, receiverUsername } = req.params;
+
+  const messages: IMessageDocument[] = await getMessages(senderUsername, receiverUsername);
+
+  res.status(StatusCodes.OK).json({
+    message: 'Get chat messages successfully',
+    messages
+  });
+}
+
+export async function userMessages(req: Request, res: Response, _next: NextFunction) {
+  const { conversationId } = req.params;
+
+  const messages: IMessageDocument[] = await getUserMessages(conversationId);
+
+  res.status(StatusCodes.OK).json({
+    message: 'Get chat messages successfully',
+    messages
+  });
+}
---
+++ src/controllers/update.ts
@@ -0,0 +1,35 @@
+import { markManyMessagesAsRead, markMessageAsRead, updateOffer } from '@chat/services/message.service';
+import { NextFunction, Request, Response } from 'express';
+import { StatusCodes } from 'http-status-codes';
+
+export async function offer(req: Request, res: Response, _next: NextFunction) {
+  const { messageId, type } = req.body;
+
+  const message = await updateOffer(messageId, type);
+
+  res.status(StatusCodes.OK).json({
+    message: 'Message updated successfully',
+    singleMessage: message
+  });
+}
+
+export async function markSingleMessage(req: Request, res: Response, _next: NextFunction) {
+  const { messageId } = req.body;
+
+  const message = await markMessageAsRead(messageId);
+
+  res.status(StatusCodes.OK).json({
+    message: 'Mark message as read successfully',
+    singleMessage: message
+  });
+}
+
+export async function markMultipleMessages(req: Request, res: Response, _next: NextFunction) {
+  const { messageId, senderUsername, receiverUsername } = req.body;
+
+  await markManyMessagesAsRead(senderUsername, receiverUsername, messageId);
+
+  res.status(StatusCodes.OK).json({
+    message: 'Mark messages as read successfully'
+  });
+}
